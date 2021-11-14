89aa9f98d69e2252daaafc9deff6a4e0710a77ea
Jade <popkilj289@gmail.com>
2021-11-14 17:19:36 -0500

    Removed redundant helper files

--- words/helper/print_words_filtered.py
+++
@@ -1,9 +0,0 @@
-from words import words_en
-import os
-
-worddict = words_en.words
-
-for word in worddict:
-    for entry in worddict[word]:
-        if 'group' in entry and 'overpowered' in entry:
-                print(entry)
