7d4354a211c13c40bf9e75e1d899b78f80a8ff80
Jade <popkilj289@gmail.com>
2021-11-14 18:04:32 -0500

    Modified wordlist to have some more one_block and one_item variables

--- words/helper/blanket/print_missing_blocktags.py
+++ words/helper/blanket/print_missing_blocktags.py
@@ -10,8 +10,15 @@ def remove_numbers(word):
         word = word.strip(i)
     return word
 
+done = 0
+total = 0
 for word in word_list:
     for entry in word_list[word]:
         if 'group' in entry and 'manual' not in entry and 'block' in entry['prereq'] and not 'one_block' in entry:
+            total += 1
             if not path.isfile('datapacks/Wordsmith/data/wordsmith/tags/blocks/{}.json'.format(remove_numbers(entry['group']))):
                 print(entry)
+            else:
+                done += 1
+
+print(f'{done} done out of {total} total')
